34c42723dfcf1e4e03763b41f201c41083387d74
gengocodes <paulcorsino28@gmail.com>
2026-03-13 11:56:32 +0800

    add mysql config

--- django_coursera/settings.py
+++ django_coursera/settings.py
@@ -28,6 +28,42 @@ DEBUG = False
 ALLOWED_HOSTS = ['*']
 
 
+# Database
+# https://docs.djangoproject.com/en/4.1/ref/settings/#databases
+#
+# JUST COMMENTED OUT THE LAB DETAILS AS I DON'T HAVE MYSQL INSTALLED
+#
+# Commands in terminal:
+# python manage.py makemigrations
+# python manage.py migrate
+# 
+# Commands inside mysql:
+# 1. mysql -u root -p
+# 2. SHOW DATABASES;
+# 3. CREATE DATABASE feedback;
+# 4. exit
+#
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'feedback',
+#         'HOST' : '127.0.0.1',
+#         'PORT' : '3306',
+#         'USER' : 'root',
+#         'PASSWORD' : '',
+#     }
+# }
+
+# INSTALLED_APPS = [
+#     'myapp.apps.MyappConfig',
+#     'django.contrib.admin',
+#     'django.contrib.auth',
+#     'django.contrib.contenttypes',
+#     'django.contrib.sessions',
+#     'django.contrib.messages',
+#     'django.contrib.staticfiles',
+# ]
+
 # Application definition
 
 INSTALLED_APPS = [
