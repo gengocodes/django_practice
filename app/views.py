bfed4f288aed3946a98a5bcdd55b32b069a2a377
gengocodes <paulcorsino28@gmail.com>
2026-03-14 12:07:08 +0800

    add templating (no images)

--- app/views.py
+++ app/views.py
@@ -18,10 +18,12 @@ def home(request):
     return HttpResponse("Welcome to Little Lemon !")
 
 def about(request):
-    return HttpResponse("About us")
+    about_content = {'about': "Little Lemon is a family-owned Mediterranean restaurant, focused on traditional recipes served with a modern twist. The chefs draw inspiration from Italian, Greek, and Turkish culture and have a menu of 12–15 items that they rotate seasonally. The restaurant has a rustic and relaxed atmosphere with moderate prices, making it a popular place for a meal any time of the day."} 
+    return render(request, "about.html", {'content': about_content})
 
 def menu(request):
-    return HttpResponse("Menu for Little Lemon")
+    about_content = {'about': "Little Lemon is a family-owned Mediterranean restaurant, focused on traditional recipes served with a modern twist. The chefs draw inspiration from Italian, Greek, and Turkish culture and have a menu of 12–15 items that they rotate seasonally. The restaurant has a rustic and relaxed atmosphere with moderate prices, making it a popular place for a meal any time of the day."} 
+    return render(request, "menu.html", {'content': about_content})
 
 def book(request):
     return HttpResponse("Make a booking")
