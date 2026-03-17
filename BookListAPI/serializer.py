d3e700522004d15e18f664d3ce69c4ebd6616abd
gengocodes <paulcorsino28@gmail.com>
2026-03-17 11:25:18 +0800

    add serialization methods

--- BookListAPI/serializer.py
+++ BookListAPI/serializer.py
@@ -5,3 +5,36 @@ class MenuItemSerializer(serializers.ModelSerializer):
     class Meta:
         model = MenuItem
         fields = ['id', 'title', 'price', 'inventory']
+
+# Method 1 Serializer:
+# from rest_framework import serializers
+# from decimal import Decimal
+# from .models import MenuItem, Category 
+# class CategorySerializer (serializers.ModelSerializer):
+#     class Meta:
+#         model = Category
+#         fields = ['id','slug','title']
+#  
+# class MenuItemSerializer(serializers.ModelSerializer):
+#     stock =  serializers.IntegerField(source='inventory')
+#     price_after_tax = serializers.SerializerMethodField(method_name = 'calculate_tax')
+#     category = CategorySerializer()
+#     class Meta:
+#         model = MenuItem
+#         fields = ['id','title','price','stock', 'price_after_tax','category']
+#     
+#     def calculate_tax(self, product:MenuItem):
+#         return product.price * Decimal(1.1)
+
+# Method 2 Serializer:
+# class MenuItemSerializer(serializers.ModelSerializer):
+#     stock =  serializers.IntegerField(source='inventory')
+#     price_after_tax = serializers.SerializerMethodField(method_name = 'calculate_tax')
+#     # category = CategorySerializer()
+#     class Meta:
+#         model = MenuItem
+#         fields = ['id','title','price','stock', 'price_after_tax','category']
+#         depth = 1
+#     
+#     def calculate_tax(self, product:MenuItem):
+#         return product.price * Decimal(1.1)
